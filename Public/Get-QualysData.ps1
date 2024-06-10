e15810e66ac341c25de6ac74d29776a701c84caf
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-06-10 15:41:53 +0200

    Add method to get custom reports

---
+++ Public/Get-QualysData.ps1
@@ -0,0 +1,18 @@
+﻿function Get-QualysData {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory)][ValidateSet(
+            'HostSummary',
+            'LocalAdmins'
+        )][string] $Type,
+        [int] $MaximumRecords
+    )
+
+    if ($Type -eq 'HostSummary') {
+        Get-QualysReportInformation -MaximumRecords $MaximumRecords
+    } elseif ($Type -eq 'LocalAdmins') {
+        Get-QualysReportLocalAdmins -MaximumRecords $MaximumRecords
+    } else {
+        throw 'Invalid Type'
+    }
+}
