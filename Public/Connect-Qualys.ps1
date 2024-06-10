0fb9a9c229fe2762254113591b65c508b6e26499
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-06-10 15:34:56 +0200

    Add Qualys connection function with multiple authentication options
    This commit introduces the ‘Connect-Qualys’ function to establish and handle API connections to Qualys. It includes support for multiple authentication methods: plaintext password, secure password, or inherited from PS credentials. Significant effort has been placed on error handling especially during password conversions, providing clear warning messages when issues arise. The outcome of this function is a reusable object for future API calls, enhancing maintainability and reducing potential for error in subsequent scripts.

---
+++ Public/Connect-Qualys.ps1
@@ -0,0 +1,35 @@
+﻿function Connect-Qualys {
+    [CmdletBinding(DefaultParameterSetName = 'Credential')]
+    param(
+        [Parameter(Mandatory, ParameterSetName = 'SecurePassword')]
+        [Parameter(Mandatory, ParameterSetName = 'Password')]
+        [Parameter(Mandatory, ParameterSetName = 'Credential')]
+        [string] $Url,
+        [Parameter(Mandatory, ParameterSetName = 'SecurePassword')]
+        [Parameter(Mandatory, ParameterSetName = 'Password')][string] $Username,
+        [Parameter(Mandatory, ParameterSetName = 'Password')][string] $Password,
+        [Parameter(Mandatory, ParameterSetName = 'Credential')][pscredential] $Credential,
+
+        [alias('SecurePassword')][Parameter(Mandatory, ParameterSetName = 'SecurePassword')][string] $EncryptedPassword
+    )
+
+    if ($EncryptedPassword) {
+        try {
+            $Password = $EncryptedPassword | ConvertTo-SecureString -ErrorAction Stop
+        } catch {
+            if ($ErrorActionPreference -eq 'Stop') {
+                throw
+            }
+            Write-Warning -Message "Connect-Qualys - Unable to convert password to secure string. Error: $($_.Exception.Message)"
+            return
+        }
+    } elseif ($Credential) {
+        $UserName = $Credential.UserName
+        $Password = $Credential.GetNetworkCredential().Password
+    }
+
+    $Script:PowerQualys = @{
+        'Uri'           = $Url
+        'Authorization' = 'Basic {0}' -f ([Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f $UserName, $Password))))
+    }
+}
